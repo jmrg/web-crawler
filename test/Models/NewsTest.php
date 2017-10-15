4f1f57d5846ad1a20d2ee7709a6708e62cf6101c
Mauricio Reina <mauricio.reina.gabela@gmail.com>
2017-10-15 15:08:34 -0500

    Creation the News model
    Creation of the News class with logic necessary for the load
    of data from the API HackerNews.

---
+++ test/Models/NewsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace WebCrawler\Test\Models;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+use WebCrawler\Models\News;
+
+/**
+ * Class NewsTest
+ * @package WebCrawler\Test\Models
+ */
+class NewsTest extends TestCase
+{
+    /**
+     * @var News
+     */
+    private $NewsModel;
+
+    protected function setUp()
+    {
+        $res = (new Client())->request(
+            'GET',
+            'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+        );
+
+        $this->NewsModel = new News($res);
+    }
+
+    /**
+     * The method getNews should return a array with the first
+     * thirty entries after make http request.
+     *
+     * @covers News::getNews()
+     */
+    public function testNewsModelShouldReturnArrayWithTheFirstThirtyEntriesNews()
+    {
+        $news = $this->NewsModel->getTop();
+
+        $this->assertEquals(is_array($news), true);
+        $this->assertEquals(count($news), 30);
+    }
+}
